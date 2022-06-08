8b89cebc157a6f631388fc61ad22cda90662614d
dickson-mbeya <dmbeya@must.ac.mw>
2022-06-08 16:53:20 -0700

    print alphabetr 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,16 @@
  * Return: Always 0
  */
 void print_alphabet_x10(void)
-{	char = 'a';
+{	char ch = 'a';
 
 	while (ch <= 'z')
 	{
-		for (int i = 0; i < 10; i++)
+		int i = 0;
+
+		while(i < 10)
 		{
 			_putchar(ch);
+			i++;
 		}
 		ch++;
 	}
