c041a9bab764635060bd191b0a5e01af8f13c8f8
dickson-mbeya <dmbeya@must.ac.mw>
2022-06-09 13:43:21 -0700

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,20 +17,23 @@ int print_sign(int n)
 
 	if ( n > 0)
 	{
-		_putchar("+");
-		_putchar(", ");
+		_putchar(43);
+		_putchar(44);
+		_putchar(32);
 		return (pstv);
 	}
 	else if (n == 0)
 	{
-		_putchar("0");
-		_putchar(", ");
+		_putchar(48);
+		_putchar(44);
+		_putchar(32);
 		return (zro);
 	}
 	else
 	{
-		_putchar("-");
-		_putchar(", ");
+		_putchar(45);
+		_putchar(44);
+		_putchar(32);
 		return (ngtv);
 	}
 }
