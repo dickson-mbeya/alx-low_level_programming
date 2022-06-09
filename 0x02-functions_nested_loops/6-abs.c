1cc786affce536b276cb02df405bddb37b467c33
dickson-mbeya <dmbeya@must.ac.mw>
2022-06-09 14:01:14 -0700

    Compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value
+ *
+ * Return: On success 1.
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else 
+	{
+		return (n * -1);
+	}
+}
