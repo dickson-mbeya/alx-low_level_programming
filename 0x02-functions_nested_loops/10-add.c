0abb297e17bb3f18e08bbe06fbeb7a7151246b8d
dickson-mbeya <dmbeya@must.ac.mw>
2022-06-09 15:19:14 -0700

    Add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - checks sign of number
+ *
+ * @a: The number to add
+ * @b: The other number to add
+ *
+ * Return: On success 1.
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
