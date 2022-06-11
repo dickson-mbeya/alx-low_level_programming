c6a4fcb35dc10040a4ccab616e4dd5df64e83b0b
dickson-mbeya <dmbeya@must.ac.mw>
2022-06-11 05:23:54 +0200

    Check digit;

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for digit
+ *
+ * @c: The charcter to check
+ *
+ * Return: On success 1.
+ * On not alphabet, 0 is returned.
+ */
+int _isdigit(int c)
+{
+
+    if (c >= 48 && c <= 57)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
