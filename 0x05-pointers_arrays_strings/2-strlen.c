230855ddd1f7d12f3d168043c604033dd87ec919
dickson-mbeya <dmbeya@must.ac.mw>
2022-06-16 22:31:18 +0200

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - finds length of string
+ * @s: string to check length
+ *
+ * Return: On success string length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s[c] != '\0')
+	{
+		c++;
+	}
+	return c;
+}
