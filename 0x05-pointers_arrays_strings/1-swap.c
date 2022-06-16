cb11f107dd7f4f6109f2cea20dda189629f4f92d
dickson-mbeya <dmbeya@must.ac.mw>
2022-06-16 22:12:22 +0200

    swaps two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int - swaps values of integers
- * @a - first iteger to swap
- * @b - second integer to swap
+ * @a: first iteger to swap
+ * @b: second integer to swap
  *
  * Return: On sucess 0.
  */
@@ -11,6 +11,7 @@ void swap_int(int *a, int *b)
 {
 	int temp;
 	temp = *a;
+
 	*a = *b;
 	*b = temp;
 }
